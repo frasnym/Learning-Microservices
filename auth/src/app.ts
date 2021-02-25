1130c8d1900c54d94712be3f835d4aa18cf68ef2
frasnym <frastyawan.nym@gmail.com>
2021-02-25 17:26:15 +0800

    fix(auth): Solve Err: Error: write EPIPE

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -3,6 +3,30 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
-app.listen(3000, () => {
-	console.log('Listening on PORT 3000!');
+app.get('/api/users/currentuser', (req, res) => {
+	res.send('Hi..!');
 });
+
+app.listen(3000, () => {
+	console.log('Listening on PORT 3000!!!');
+});
+
+/**
+ * !Solve Err: Error: write EPIPE
+ */
+process.stdout.on('error', function (err) {
+	if (err.code == 'EPIPE') {
+		process.exit(0);
+	}
+});
+
+/**
+ * TODO
+ * [auth] npm ERR! path /app
+ * [auth] npm ERR! command failed
+ * [auth] npm ERR! signal SIGKILL
+ * [auth] npm ERR! command sh -c ts-node-dev ./src/app.ts
+ * [auth]
+ * [auth] npm ERR! A complete log of this run can be found in:
+ * [auth] npm ERR!     /root/.npm/_logs/2021-02-25T09_08_39_044Z-debug.log
+ */
