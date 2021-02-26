8e852c84598727ab2e04f27abcd99460daa42e36
frasnym <frastyawan.nym@gmail.com>
2021-02-26 16:12:45 +0800

    fix(user): Property 'build' comes from an index signature, so it must be accessed with ['build']

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -37,7 +37,7 @@ const userSchema = new mongoose.Schema({
 	},
 });
 
-userSchema.statics.build = (attrs: UserAttrs) => {
+userSchema.statics['build'] = (attrs: UserAttrs) => {
 	return new User(attrs);
 };
 
