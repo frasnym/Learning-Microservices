f94a6f7e87388e3423922d64682e3261442b4882
frasnym <frastyawan.nym@gmail.com>
2021-02-26 12:33:23 +0800

    feat(auth-signup): Add body validator

--- auth/src/routes/users.ts
+++ auth/src/routes/users.ts
@@ -1,9 +1,23 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.route('/currentuser').get((req, res) => {
-	res.send('hi?');
-});
+router
+	.route('/signup')
+	.post(
+		[
+			body('email').isEmail().withMessage('Email must be valid'),
+			body('password')
+				.trim()
+				.isLength({ min: 4, max: 20 })
+				.withMessage('Password must be between 4 and 20 character'),
+		],
+		(req: Request, res: Response) => {
+			const { email, password } = req.body;
+
+			res.send('ok signup');
+		}
+	);
 
 export { router as usersRouter };
