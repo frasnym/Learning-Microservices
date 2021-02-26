125f4db867d52d97d6139a45ecdc1abb17f4f993
frasnym <frastyawan.nym@gmail.com>
2021-02-26 13:23:41 +0800

    fix: solve signal SIGKILL by remove containers limits

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -9,23 +9,3 @@ app.use('/api/users', usersRouter);
 app.listen(3000, () => {
 	console.log('Listening on PORT 3000');
 });
-
-/**
- * !Solve Err: Error: write EPIPE
- */
-process.stdout.on('error', function (err) {
-	if (err.code == 'EPIPE') {
-		process.exit(0);
-	}
-});
-
-/**
- * TODO
- * [auth] npm ERR! path /app
- * [auth] npm ERR! command failed
- * [auth] npm ERR! signal SIGKILL
- * [auth] npm ERR! command sh -c ts-node-dev ./src/app.ts
- * [auth]
- * [auth] npm ERR! A complete log of this run can be found in:
- * [auth] npm ERR!     /root/.npm/_logs/2021-02-25T09_08_39_044Z-debug.log
- */
