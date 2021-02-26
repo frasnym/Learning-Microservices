d10398e9376a83c283735a3e370a8eb6fd3ce6ab
frasnym <frastyawan.nym@gmail.com>
2021-02-26 15:17:22 +0800

    feat(auth-db): Connecting to MongoDB

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
+import mongoose from 'mongoose';
+
 import { NotFoundError } from './errors/not-found-error';
 import { errorHandler } from './middlewares/error-handler';
 import { usersRouter } from './routes/users';
@@ -13,6 +15,20 @@ app.all('*', async () => {
 });
 app.use(errorHandler);
 
-app.listen(3000, () => {
-	console.log('Listening on PORT 3000!');
-});
+const start = async () => {
+	try {
+		await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+		});
+		console.log('Connected to MongoDB');
+	} catch (error) {
+		console.log(error);
+	}
+
+	app.listen(3000, () => {
+		console.log('Listening on PORT 3000!');
+	});
+};
+start();
