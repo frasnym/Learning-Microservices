1097b6707a053e7786f6218b2e776f1863b95a5c
frasnym <frastyawan.nym@gmail.com>
2021-02-26 16:17:46 +0800

    feat(user-signup): Create an User

--- auth/src/routes/users.ts
+++ auth/src/routes/users.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
 import { RequestValidationError } from '../errors/request-validation-error';
+import { User } from '../models/user';
 
 const router = express.Router();
 
@@ -15,16 +15,24 @@ router
 				.isLength({ min: 4, max: 20 })
 				.withMessage('Password must be between 4 and 20 character'),
 		],
-		(req: Request, res: Response) => {
+		async (req: Request, res: Response) => {
 			const errors = validationResult(req);
 			if (!errors.isEmpty()) {
 				throw new RequestValidationError(errors.array());
 			}
 
-			console.log('Creating user...');
-			throw new DatabaseConnectionError();
+			const { email, password } = req.body;
 
-			return res.send('ok signup');
+			const existingUser = await User.findOne({ email });
+			if (existingUser) {
+				console.log('Email in use');
+				return res.send({});
+			}
+
+			const user = User.build({ email, password });
+			await user.save();
+
+			return res.status(201).send(user);
 		}
 	);
 
