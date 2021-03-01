2dd9f613db9923202da218d7f3926f5de5e4b6f6
frasnym <frastyawan.nym@gmail.com>
2021-03-01 15:37:08 +0800

    feat(client-signup): Handle error

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,9 +1,10 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import axios from 'axios';
 
 export default function signup() {
 	const emailInputRef = useRef('');
 	const passwordInputRef = useRef('');
+	const [errors, setErrors] = useState([]);
 
 	const onSubmit = async (event) => {
 		event.preventDefault();
@@ -11,12 +12,16 @@ export default function signup() {
 		const email = emailInputRef.current.value;
 		const password = passwordInputRef.current.value;
 
-		const response = await axios.post('/api/users/signup', {
-			email,
-			password,
-		});
+		try {
+			const response = await axios.post('/api/users/signup', {
+				email,
+				password,
+			});
 
-		console.log(response.data);
+			console.log(response.data);
+		} catch (error) {
+			setErrors(error.response.data.errors);
+		}
 	};
 
 	return (
@@ -34,6 +39,16 @@ export default function signup() {
 					className="form-control"
 				/>
 			</div>
+			{errors.length > 0 && (
+				<div className="alert alert-danger">
+					<h4>Ops...</h4>
+					<ul className="my-0">
+						{errors.map((e) => (
+							<li key={e.message}>{e.message}</li>
+						))}
+					</ul>
+				</div>
+			)}
 			<button className="btn btn-primary">Sign Up</button>
 		</form>
 	);
