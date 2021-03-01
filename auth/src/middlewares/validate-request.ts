cf6e737921ab046d4b017eab39b822bafcc4236e
frasnym <frastyawan.nym@gmail.com>
2021-03-01 12:14:54 +0800

    feat(auth): Add middleware to validate request

---
+++ auth/src/middlewares/validate-request.ts
@@ -0,0 +1,12 @@
+import { RequestHandler } from 'express';
+import { validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+
+export const validateRequest: RequestHandler = (req, _res, next) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		throw new RequestValidationError(errors.array());
+	}
+
+	next();
+};
