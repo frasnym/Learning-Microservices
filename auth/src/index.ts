c12df03a895d4545b0f4a1b70007460579548af5
frasnym <frastyawan.nym@gmail.com>
2021-03-01 11:11:16 +0800

    feat(auth): set config for cookie

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,21 @@
 import express from 'express';
 import 'express-async-errors';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { NotFoundError } from './errors/not-found-error';
 import { errorHandler } from './middlewares/error-handler';
 import { usersRouter } from './routes/users';
 
 const app = express();
+app.set('trust proxy', true); // trust connection from ingress
 app.use(express.json());
+app.use(
+	cookieSession({
+		signed: false, // disable encryption
+		secure: true, // enable only on https connection
+	})
+);
 
 app.use('/api/users', usersRouter);
 app.all('*', async () => {
