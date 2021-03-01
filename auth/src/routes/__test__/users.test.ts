7d63cc35704455da4185b37237d50045cd81c835
frasnym <frastyawan.nym@gmail.com>
2021-03-01 13:54:31 +0800

    test(auth-signup): Ensure SignUp returns 400 with if invalid email provided

--- auth/src/routes/__test__/users.test.ts
+++ auth/src/routes/__test__/users.test.ts
@@ -1,8 +1,8 @@
 import request from 'supertest';
 import { app } from '../../app';
 
-describe('usersRouter', () => {
-	test('should returns a 201 on successful signup', async () => {
+describe('usersRouter - Sign Up', () => {
+	test('should returns 201 on successful signup', async () => {
 		return request(app)
 			.post('/api/users/signup')
 			.send({
@@ -10,5 +10,15 @@ describe('usersRouter', () => {
 				password: 'password',
 			})
 			.expect(201);
+	});
+
+	test('should returns 400 with if invalid email provided', async () => {
+		return request(app)
+			.post('/api/users/signup')
+			.send({
+				email: 'invalid_email',
+				password: 'password',
+			})
+			.expect(400);
 	});
 });
