ac144038de42862d0fb8c1d2dec6c0d7d9e06e6f
frasnym <frastyawan.nym@gmail.com>
2021-03-01 13:57:21 +0800

    test(auth-signup): Ensure returns 400 with if no email and password provided

--- auth/src/routes/__test__/users.test.ts
+++ auth/src/routes/__test__/users.test.ts
@@ -31,4 +31,15 @@ describe('usersRouter - Sign Up', () => {
 			})
 			.expect(400);
 	});
+
+	test('should returns 400 with if no email and password provided', async () => {
+		await request(app)
+			.post('/api/users/signup')
+			.send({ email: 'test@test.com' })
+			.expect(400);
+		await request(app)
+			.post('/api/users/signup')
+			.send({ password: 'password' })
+			.expect(400);
+	});
 });
