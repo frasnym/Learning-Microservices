1b3824075471c16c8827ae936d26371b5cb07450
frasnym <frastyawan.nym@gmail.com>
2021-03-01 15:25:07 +0800

    feat(client-signup): Add submitHandler

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 export default function signup() {
+	const emailInputRef = useRef('');
+	const passwordInputRef = useRef('');
+
+	const onSubmit = (event) => {
+		event.preventDefault();
+
+		console.log(
+			emailInputRef.current.value,
+			passwordInputRef.current.value
+		);
+	};
+
 	return (
-		<form>
+		<form onSubmit={onSubmit}>
 			<h1>Sign Up</h1>
 			<div className="form-group">
 				<label>Email Address</label>
-				<input className="form-control" />
+				<input ref={emailInputRef} className="form-control" />
 			</div>
 			<div className="form-group">
 				<label>Password</label>
-				<input type="password" className="form-control" />
+				<input
+					ref={passwordInputRef}
+					type="password"
+					className="form-control"
+				/>
 			</div>
 			<button className="btn btn-primary">Sign Up</button>
 		</form>
