2716c4e12a34cdab72a0fd60bed0a4088f09a0ea
frasnym <frastyawan.nym@gmail.com>
2021-03-01 13:23:18 +0800

    refactor(auth): index to app

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,27 +1,5 @@
-import express from 'express';
-import 'express-async-errors';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-import { NotFoundError } from './errors/not-found-error';
-import { errorHandler } from './middlewares/error-handler';
-import { usersRouter } from './routes/users';
-
-const app = express();
-app.set('trust proxy', true); // trust connection from ingress
-app.use(express.json());
-app.use(
-	cookieSession({
-		signed: false, // disable encryption
-		secure: true, // enable only on https connection
-	})
-);
-
-app.use('/api/users', usersRouter);
-app.all('*', async () => {
-	throw new NotFoundError();
-});
-app.use(errorHandler);
+import { app } from './app';
 
 const start = async () => {
 	if (!process.env.JWT_KEY) {
