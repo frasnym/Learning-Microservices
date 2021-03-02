a8024766563325cae179a4bbed2b8e0803c60df8
frasnym <frastyawan.nym@gmail.com>
2021-03-02 11:54:23 +0800

    feat(client-header): Conditionally show links

--- client/components/header.js
+++ client/components/header.js
@@ -2,6 +2,31 @@ import React from 'react';
 import Link from 'next/link';
 
 export default function Header({ currentUser }) {
+	const links = [
+		!currentUser && {
+			label: 'Sign Up',
+			href: '/auth/signup',
+		},
+		!currentUser && {
+			label: 'Sign In',
+			href: '/auth/signin',
+		},
+		currentUser && {
+			label: 'Sign Out',
+			href: '/auth/signout',
+		},
+	]
+		.filter((linkConfig) => linkConfig)
+		.map(({ label, href }) => {
+			return (
+				<li key={href} className="nav-item">
+					<Link href={href}>
+						<a className="nav-link">{label}</a>
+					</Link>
+				</li>
+			);
+		});
+
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<Link href="/">
@@ -9,9 +34,7 @@ export default function Header({ currentUser }) {
 			</Link>
 
 			<div className="d-flex justify-content-end">
-				<ul className="nav d-flet align-items-center">
-					{currentUser ? 'Sign Out' : 'Sign In/Up'}
-				</ul>
+				<ul className="nav d-flet align-items-center">{links}</ul>
 			</div>
 		</nav>
 	);
