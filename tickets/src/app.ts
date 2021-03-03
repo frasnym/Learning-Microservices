3bce454b3e1e93ba354e9da7fbc704024e4eff74
frasnym <frastyawan.nym@gmail.com>
2021-03-03 09:47:00 +0800

    chore(tickets): Initial setup

---
+++ tickets/src/app.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import 'express-async-errors';
+import cookieSession from 'cookie-session';
+
+import { NotFoundError, errorHandler } from '@frntickets/common';
+
+const app = express();
+app.set('trust proxy', true); // trust connection from ingress
+app.use(express.json());
+app.use(
+	cookieSession({
+		signed: false, // disable encryption
+		secure: process.env.NODE_ENV !== 'test', // enable only on https connection on "Production" or "Development"
+	})
+);
+
+app.all('*', async () => {
+	throw new NotFoundError();
+});
+app.use(errorHandler);
+
+export { app };
