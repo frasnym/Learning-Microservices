4a1dbe7d4aa9da6d72e805e69d854818269b53b1
frasnym <frastyawan.nym@gmail.com>
2021-03-03 10:21:54 +0800

    test(tickets): TDD approach

---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,14 @@
+// import request from 'supertest';
+// import { app } from '../../app';
+
+describe('New Routes', () => {
+	test('should has a route handler listening to /api/tickets for POST method', async () => {});
+
+	test('should can be accessed if the user is signed in', async () => {});
+
+	test('should returns an error if an invalid title is provided', async () => {});
+
+	test('should returns an error if an invalid price is provided', async () => {});
+
+	test('should successfully create a ticket with valid input provided', async () => {});
+});
