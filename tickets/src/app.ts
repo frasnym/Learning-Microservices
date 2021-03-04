cb0384425340a741fe00f9848277a2e7c3e507f6
frasnym <frastyawan.nym@gmail.com>
2021-03-04 10:51:03 +0800

    test(tickets-show): Ensure returns a tickets if the ticket is found

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,6 +4,7 @@ import cookieSession from 'cookie-session';
 
 import { NotFoundError, errorHandler, currentUser } from '@frntickets/common';
 import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
 
 const app = express();
 app.set('trust proxy', true); // trust connection from ingress
@@ -16,6 +17,7 @@ app.use(
 );
 app.use(currentUser);
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all('*', async () => {
 	throw new NotFoundError();
---
+++ tickets/src/routes/__test__/show.test.ts
@@ -0,0 +1,27 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+describe('Show routes', () => {
+	test('should returns a 404 if the ticket is not found', async () => {
+		const invalidId = 'invalid_id';
+		await request(app).get(`/api/tickets/${invalidId}`).expect(404);
+	});
+
+	test('should returns a tickets if the ticket is found', async () => {
+		const title = 'valid_title';
+		const price = 20;
+
+		const response = await request(app)
+			.post('/api/tickets')
+			.set('Cookie', global.signin())
+			.send({ title, price })
+			.expect(201);
+
+		const ticketResponse = await request(app)
+			.get(`/api/tickets/${response.body.id}`)
+			.expect(200);
+
+		expect(ticketResponse.body.title).toBe(title);
+		expect(ticketResponse.body.price).toBe(price);
+	});
+});
