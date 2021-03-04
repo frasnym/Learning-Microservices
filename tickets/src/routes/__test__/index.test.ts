f36649bfad3262a5760d50847ce0a10280c1d7a4
frasnym <frastyawan.nym@gmail.com>
2021-03-04 11:28:15 +0800

    test(tickets-index): Ensure fetch a list of tickets

---
+++ tickets/src/routes/__test__/index.test.ts
@@ -0,0 +1,21 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+const createTicket = () => {
+	return request(app)
+		.post('/api/tickets')
+		.set('Cookie', global.signin())
+		.send({ title: 'valid_title', price: 20 });
+};
+
+describe('Index route', () => {
+	test('should fetch a list of tickets', async () => {
+		await createTicket();
+		await createTicket();
+		await createTicket();
+
+		const response = await request(app).get('/api/tickets').expect(200);
+
+		expect(response.body.length).toBe(3);
+	});
+});
