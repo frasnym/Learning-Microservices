ee05b7f306d13da16b10c4d2701caebd15f4f183
frasnym <frastyawan.nym@gmail.com>
2021-03-04 11:37:35 +0800

    test(tickets-update): Test First Approach

---
+++ tickets/src/routes/__test__/update.test.ts
@@ -0,0 +1,10 @@
+// import request from 'supertest';
+// import { app } from '../../app';
+
+describe('Update route', () => {
+	test.todo('should returns a 404 if the provided id does not exist');
+	test.todo('should a 401 if the user is not authenticated');
+	test.todo("should a 401 if the user doesn't own the ticket");
+	test.todo('should a 400 if invalid title or price provided');
+	test.todo('should updates the ticket with valid value provided');
+});
