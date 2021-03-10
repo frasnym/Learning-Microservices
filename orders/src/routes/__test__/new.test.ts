311a3a5dcf566cea7b291c7f45746c52c7c5cc25
frasnym <frastyawan.nym@gmail.com>
2021-03-10 11:29:54 +0800

    test(orders): Ensure returns bad request error if the ticket is already reserved

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../../app';
+import { Order, OrderStatus } from '../../models/order';
+import { Ticket } from '../../models/ticket';
 
 describe('Order New Routes', () => {
 	test('should returns not found error if the ticket does not exist', async () => {
@@ -14,8 +16,30 @@ describe('Order New Routes', () => {
 			})
 			.expect(404);
 	});
-	test.todo(
-		'should returns bad request error if the ticket is already reserved'
-	);
+
+	test('should returns bad request error if the ticket is already reserved', async () => {
+		const ticket = Ticket.build({
+			title: 'concert',
+			price: 20,
+		});
+		await ticket.save();
+
+		const order = Order.build({
+			ticket: ticket,
+			userId: 'randomid',
+			status: OrderStatus.Created,
+			expiresAt: new Date(),
+		});
+		await order.save();
+
+		await request(app)
+			.post('/api/orders')
+			.set('Cookie', global.signin())
+			.send({
+				ticketId: ticket.id,
+			})
+			.expect(400);
+	});
+
 	test.todo('should sucessfully reserved a ticket');
 });
