4b687bd15809c2cb58fa3b193d184150941de844
frasnym <frastyawan.nym@gmail.com>
2021-03-10 09:26:22 +0800

    feat(orders-new): Create validation on incoming request

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,23 @@
+import { requireAuth, validateRequest } from '@frntickets/common';
 import { Request, Response, Router } from 'express';
+import { body } from 'express-validator';
+import mongoose from 'mongoose';
 
 const router = Router();
 
-router.post('/api/orders', async (_req: Request, res: Response) => {
-	res.send({});
-});
+router.post(
+	'/api/orders',
+	requireAuth,
+	[
+		body('ticketId')
+			.notEmpty()
+			.custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+			.withMessage('TicketId must be provided'),
+	],
+	validateRequest,
+	async (_req: Request, res: Response) => {
+		res.send({});
+	}
+);
 
 export { router as newOrderRouter };
