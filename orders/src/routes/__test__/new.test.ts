8f558e5ed4d3272c3713d535ee3672cd2ade3b89
frasnym <frastyawan.nym@gmail.com>
2021-03-10 11:24:46 +0800

    test(orders): Ensure returns not found error if the ticket does not exist

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -1,5 +1,21 @@
+import mongoose from 'mongoose';
+import request from 'supertest';
+import { app } from '../../app';
+
 describe('Order New Routes', () => {
-	test.todo('should returns error if the ticket does not exist');
-	test.todo('should returns error if the ticket is already reserved');
+	test('should returns not found error if the ticket does not exist', async () => {
+		const ticketId = mongoose.Types.ObjectId();
+
+		await request(app)
+			.post('/api/orders')
+			.set('Cookie', global.signin())
+			.send({
+				ticketId,
+			})
+			.expect(404);
+	});
+	test.todo(
+		'should returns bad request error if the ticket is already reserved'
+	);
 	test.todo('should sucessfully reserved a ticket');
 });
