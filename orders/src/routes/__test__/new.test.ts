261afe4fd34e06f8699ea8aa3063f068db40f319
frasnym <frastyawan.nym@gmail.com>
2021-03-10 11:21:49 +0800

    test(orders): Test first approach

---
+++ orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,5 @@
+describe('Order New Routes', () => {
+	test.todo('should returns error if the ticket does not exist');
+	test.todo('should returns error if the ticket is already reserved');
+	test.todo('should sucessfully reserved a ticket');
+});
