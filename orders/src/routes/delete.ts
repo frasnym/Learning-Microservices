c7ad261ec1e22d3a20c6286072355adb7acfd0e6
frasnym <frastyawan.nym@gmail.com>
2021-03-10 13:51:47 +0800

    feat(orders-delete): Cancelling an order

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,31 @@
+import {
+	NotAuthorizedError,
+	NotFoundError,
+	OrderStatus,
+	requireAuth,
+} from '@frntickets/common';
 import { Request, Response, Router } from 'express';
+import { Order } from '../models/order';
 
 const router = Router();
 
-router.delete('/api/orders/:orderId', async (_req: Request, res: Response) => {
-	res.send({});
-});
+router.delete(
+	'/api/orders/:orderId',
+	requireAuth,
+	async (req: Request, res: Response) => {
+		const order = await Order.findById(req.params.orderId);
+		if (!order) {
+			throw new NotFoundError();
+		}
+		if (order.id !== req.currentUser?.id) {
+			throw new NotAuthorizedError();
+		}
+
+		order.status = OrderStatus.Cancelled;
+		await order.save();
+
+		res.status(204).send(order);
+	}
+);
 
 export { router as deleteOrderRouter };
