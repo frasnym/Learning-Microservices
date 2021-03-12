0b4e3f712fdbb5a5a147fa060759e15fafac7e01
frasnym <frastyawan.nym@gmail.com>
2021-03-12 09:59:42 +0800

    test(orders-delete): Ensure emits an order:cancelled event

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -3,6 +3,7 @@ import request from 'supertest';
 import { app } from '../../app';
 import { Order } from '../../models/order';
 import { Ticket } from '../../models/ticket';
+import { natsWrapper } from '../../nats-wrapper';
 
 describe('Delete Order Route', () => {
 	test('should marks order as cancelled', async () => {
@@ -30,5 +31,27 @@ describe('Delete Order Route', () => {
 		expect(updatedOrder!.status).toBe(OrderStatus.Cancelled);
 	});
 
-	test.todo('should emits an order:cancelled event');
+	test('should emits an order:cancelled event', async () => {
+		const ticket = await Ticket.build({
+			title: 'concert',
+			price: 20,
+		});
+		await ticket.save();
+
+		const user = global.signin();
+		const { body: order } = await request(app)
+			.post('/api/orders')
+			.set('Cookie', user)
+			.send({
+				ticketId: ticket.id,
+			})
+			.expect(201);
+
+		await request(app)
+			.delete(`/api/orders/${order.id}`)
+			.set('Cookie', user)
+			.expect(204);
+
+		expect(natsWrapper.client.publish).toHaveBeenCalled();
+	});
 });
