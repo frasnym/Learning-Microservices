7ab58791dd2f94112e7d2917feb70de503d1d928
frasnym <frastyawan.nym@gmail.com>
2021-03-12 11:32:09 +0800

    test(tickets): Ensure implements optimistic concurrency control

---
+++ tickets/src/models/__test__/ticket.test.ts
@@ -0,0 +1,26 @@
+import { Ticket } from '../ticket';
+
+test('should implements optimistic concurrency control', async (done) => {
+	const ticket = Ticket.build({
+		title: 'concert',
+		price: 5,
+		userId: '123',
+	});
+	await ticket.save();
+
+	const firstInstance = await Ticket.findById(ticket.id);
+	const secondInstance = await Ticket.findById(ticket.id);
+
+	firstInstance!.set({ price: 10 });
+	secondInstance!.set({ price: 15 });
+
+	await firstInstance!.save();
+
+	try {
+		await secondInstance!.save();
+	} catch (error) {
+		return done();
+	}
+
+	throw new Error('Should not react this error');
+});
