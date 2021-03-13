b522f80e30a5601d6bd6ca787df79ecf4200fd51
frasnym <frastyawan.nym@gmail.com>
2021-03-13 10:49:02 +0800

    test(tickets-listener): Ensure publish a ticket:update event

--- tickets/src/events/listeners/__test__/order-created-listener.test.ts
+++ tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -51,4 +51,16 @@ describe('Order Created Listener', () => {
 
 		expect(msg.ack).toHaveBeenCalled();
 	});
+
+	test('should publish a ticket:update event', async () => {
+		const { listener, data, msg } = await setup();
+		await listener.onMessage(data, msg);
+
+		expect(natsWrapper.client.publish).toHaveBeenCalled();
+
+		const ticketUpdatedData = JSON.parse(
+			(natsWrapper.client.publish as jest.Mock).mock.calls[0][1]
+		);
+		expect(ticketUpdatedData.orderId).toBe(data.id);
+	});
 });
