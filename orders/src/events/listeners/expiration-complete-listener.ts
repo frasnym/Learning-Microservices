1927d4943cf2001780d786a9bcca8bf6792ec643
frasnym <frastyawan.nym@gmail.com>
2021-03-23 16:34:37 +0800

    feat(orders): Handle expiration event

---
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,26 @@
+import {
+	ExpirationCompleteEvent,
+	Listener,
+	OrderStatus,
+	Subjects,
+} from '@frntickets/common';
+import { Message } from 'node-nats-streaming';
+import { Order } from '../../models/order';
+import { queueGroupName } from './queue-group-name';
+
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+	subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+	queueGroupName = queueGroupName;
+
+	async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
+		const order = await Order.findById(data.orderId);
+		if (!order) {
+			return console.error('Order not found!');
+		}
+
+		order.set({
+			status: OrderStatus.Cancelled,
+			ticket: null,
+		});
+	}
+}
