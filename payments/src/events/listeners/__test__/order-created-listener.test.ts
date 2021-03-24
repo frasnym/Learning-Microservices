4e1c1f2ad03ccaff403deacb4883528cf932927e
frasnym <frastyawan.nym@gmail.com>
2021-03-24 14:53:10 +0800

    test(payments): Ensure replicates the order info

---
+++ payments/src/events/listeners/__test__/order-created-listener.test.ts
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+import { OrderCreatedEvent, OrderStatus } from '@frntickets/common';
+import { Message } from 'node-nats-streaming';
+import { Order } from '../../../models/order';
+import { natsWrapper } from '../../../nats-wrapper';
+import { OrderCreatedListener } from '../order-created-listener';
+
+const setup = async () => {
+	const listener = new OrderCreatedListener(natsWrapper.client);
+
+	const data: OrderCreatedEvent['data'] = {
+		id: mongoose.Types.ObjectId().toHexString(),
+		status: OrderStatus.Created,
+		userId: 'randomID',
+		version: 0,
+		expiresAt: 'randomDate',
+		ticket: {
+			id: 'randomID',
+			price: 10,
+		},
+	};
+
+	// @ts-ignore
+	const msg: Message = {
+		ack: jest.fn(),
+	};
+
+	return { listener, data, msg };
+};
+
+describe('Order Created Listener', () => {
+	test('should replicates the order info', async () => {
+		const { listener, data, msg } = await setup();
+		await listener.onMessage(data, msg);
+
+		const order = await Order.findById(data.id);
+
+		expect(order?.price).toBe(data.ticket.price);
+	});
+
+	test.todo('should acks the message');
+});
