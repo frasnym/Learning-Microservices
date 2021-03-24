38b1b659f251b9bdfce9fc3e772eb97716053ee9
frasnym <frastyawan.nym@gmail.com>
2021-03-24 14:27:33 +0800

    feat(payments): Initial setup

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -9,6 +9,7 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
 	async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
 		const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+		console.log('Will be publishing expiration event after:', delay, 'ms');
 
 		await expirationQueue.add(
 			{
